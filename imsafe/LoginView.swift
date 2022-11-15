856e4108750b02d9e45c6fb70ddb8f5d4366e96a
ivoryttang <ivoryttang.com>
2022-11-15 16:12:03 -0500

    fixed login ui

--- imsafe/LoginView.swift
+++ imsafe/LoginView.swift
@@ -22,38 +22,35 @@ struct LoginView: View {
         NavigationView{
             ZStack{
                 beige.ignoresSafeArea()
-                Circle().scale(1).foregroundColor(.white.opacity(0.4))
-                Circle().scale(0.8).foregroundColor(.white.opacity(0.9))
                 VStack(spacing:10){
-                    NavigationView{
-                        Picker(selection: $isLoginMode, label: Text("Picker here")){
-                            Text("Login").tag(true)
-                            Text("Create Account").tag(false)
-                        }.pickerStyle(SegmentedPickerStyle())
+                Picker(selection: $isLoginMode, label: Text("Picker here")){
+                        Text("Login").tag(true)
+                        Text("Create Account").tag(false)
+                    }.pickerStyle(SegmentedPickerStyle())
+                
+                if isLoginMode{
+                    TextField("Username", text: $username).padding()
+                        .frame(width: 300, height: 50).background(Color.black.opacity(0.2)).cornerRadius(10).border(.red, width:CGFloat(wrongUsername))
+                    SecureField("Password", text: $password).padding().frame(width: 300, height: 50).background(Color.black.opacity(0.2)).cornerRadius(10).border(.red, width:CGFloat(wrongPassword))
+                    Button("Login"){
+                        authenticateUser(username:username, password:password)
+                    }.foregroundColor(.white).frame(width: 300, height: 50).background(yellow).cornerRadius(10)
+                        
+                    NavigationLink(destination: ContentView(username: username, date: Date()),isActive: $showingLoginScreen){
                     }
-                    if isLoginMode{
-                        TextField("Username", text: $username).padding()
-                            .frame(width: 300, height: 50).background(Color.black.opacity(0.2)).cornerRadius(10).border(.red, width:CGFloat(wrongUsername))
-                        SecureField("Password", text: $password).padding().frame(width: 300, height: 50).background(Color.black.opacity(0.2)).cornerRadius(10).border(.red, width:CGFloat(wrongPassword))
-                        Button("Login"){
-                            authenticateUser(username:username, password:password)
-                        }.foregroundColor(.white).frame(width: 300, height: 50).background(yellow).cornerRadius(10)
-                            
-                        NavigationLink(destination: ContentView(username: username, date: Date()),isActive: $showingLoginScreen){
-                        }
-                    }else{
-                        TextField("Username", text: $username).padding()
-                            .frame(width: 300, height: 50).background(Color.black.opacity(0.2)).cornerRadius(10).border(.red, width:CGFloat(wrongUsername))
-                        TextField("Email", text: $email).padding()
-                            .frame(width: 300, height: 50).background(Color.black.opacity(0.2)).cornerRadius(10)
-                        SecureField("Password", text: $password).padding().frame(width: 300, height: 50).background(Color.black.opacity(0.2)).cornerRadius(10).border(.red, width:CGFloat(wrongPassword))
-                        SecureField("Confirm Password", text: $password).padding().frame(width: 300, height: 50).background(Color.black.opacity(0.2)).cornerRadius(10).border(.red, width:CGFloat(wrongPassword))
-                        Button("Create Account"){
-                            authenticateUser(username:username, password:password)
-                        }.foregroundColor(.white).frame(width: 300, height: 50).background(yellow).cornerRadius(10)
-                    }
+                }else{
+                    TextField("Username", text: $username).padding()
+                        .frame(width: 300, height: 50).background(Color.black.opacity(0.2)).cornerRadius(10).border(.red, width:CGFloat(wrongUsername))
+                    TextField("Email", text: $email).padding()
+                        .frame(width: 300, height: 50).background(Color.black.opacity(0.2)).cornerRadius(10)
+                    SecureField("Password", text: $password).padding().frame(width: 300, height: 50).background(Color.black.opacity(0.2)).cornerRadius(10).border(.red, width:CGFloat(wrongPassword))
+                    SecureField("Confirm Password", text: $password).padding().frame(width: 300, height: 50).background(Color.black.opacity(0.2)).cornerRadius(10).border(.red, width:CGFloat(wrongPassword))
+                    Button("Create Account"){
+                        authenticateUser(username:username, password:password)
+                    }.foregroundColor(.white).frame(width: 300, height: 50).background(yellow).cornerRadius(10)
+                }
                     
-                }.offset(y:-250).navigationTitle(isLoginMode ? "Login":"Create Account")
+                }
             }
         }
          
